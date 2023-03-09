b598e4e5538539cc50a860257e63938cfd9f5399
Javier Torres <jtorres@truora.com>
2023-03-08 20:04:57 -0500

    Base forge project

--- README.md
+++
@@ -1,1 +0,0 @@
-# Costamod - Mod de Minecraft basado en la costa atlántica Colombiana - 1.18.2
--- src/main/java/io/github/javtor/costamod/world/gen/ModWorldGen.java
+++
@@ -1,8 +0,0 @@
-package io.github.javtor.costamod.world.gen;
-
-public class ModWorldGen {
-
-    public static void generateModWorldGen() {
-        ModEntitySpawn.addEntitySpawn();
-    }
-}
